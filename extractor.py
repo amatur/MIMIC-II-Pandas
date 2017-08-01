7292683575c19948cef9ec6785aaacf2d45ea3d6
amatur <amatur003@gmail.com>
2017-08-02 01:53:02 +0600

    after first example making

--- extractor.py
+++ extractor.py
@@ -9,21 +9,61 @@ def get_table_mimic(padj, table_name):
     path_mimicroot = ""
     return path_mimicroot + padj + "/" + "{0}/{1}-{0}.txt".format(pid, table_name)   
 
+def filler(x):
+    return x
+
+'''
+Physiological variables init
+'''
 
 class PhysioVar:
-    """A simple example class"""
-    def __init__(self, id, min, max, unit):
+    """A physiological variable class"""
+    def __init__(self, id, min, max, unit, name):
         self.id = id
         self.min = min
         self.max = max
+        self.name = name
 
-v_resp = PhysioVar(618, 0, 250, 'bpm')    
+vararr = []
+vararr.append(PhysioVar(618, 0, 250, 'bpm', 'RESP'))   
+vararr.append(PhysioVar(211, 0, 250, 'bpm', 'HEART'))  
+vararr.append(PhysioVar(676, 25, 42, 'c', 'TEMP'))  
+##null
+#vararr.append(PhysioVar(3834, 400, 5000, '1000 cells per micro L', 'WBC'))   
+vararr.append(PhysioVar(1162, 4, 500, 'mg per dL', 'BUN'))   
+vararr.append(PhysioVar(791, 0.1, 9, 'mg per dL', 'CREAT'))   
+#vararr.append(PhysioVar(2633, 90, 180, 'mmHg', 'SYSBP'))   
+#vararr.append(PhysioVar(2632, 60, 110, 'mmHg', 'DIABP')) 
+vararr.append(PhysioVar(52, 70, 110, 'mmHg', 'MEANBP')) 
+vararr.append(PhysioVar(646, 60, 100, '%', 'O2SAT')) 
+vararr.append(PhysioVar(818, 0, 10, 'mg per dL', 'LACTIC'))   
+#vararr.append(PhysioVar(6256, 3000, 1000000, 'cells per L', 'PLAT'))   
+#vararr.append(PhysioVar(6256, -1, 99999999999, 'cells per L', 'PLAT'))   
+#vararr.append(PhysioVar(833, 2000, 8000, 'mg per dL', 'RBC'))   
+vararr.append(PhysioVar(813, 19, 60, '%', 'HEMATOCRIT'))   
+vararr.append(PhysioVar(837, 120, 160, 'mEq per dL', 'SODIUM'))   
+vararr.append(PhysioVar(829, 2.2, 8, 'mEq per dL', 'POTASSIUM'))   
+vararr.append(PhysioVar(786, 4.8, 12, 'mg per dL', 'CALCIUM'))   
+vararr.append(PhysioVar(821, 0, 10, 'mg per dL', 'MAGNESIUM'))   
+vararr.append(PhysioVar(772, 0.5, 18, 'mg per dL', 'ALBUMIN'))   
+vararr.append(PhysioVar(780, 6.8, 7.8, ' ', 'ARTERIALPH'))   
+#vararr.append(PhysioVar(3750, 0, 1000, ' ', 'URINEOUTFOLEY'))   
+vararr.append(PhysioVar(763, 20, 200, 'kg', 'WEIGHT'))   
+#vararr.append(PhysioVar(3750, 6.8, 7.8, ' ', 'MECHANICALVENTILATION'))   
+
+
+for i in range(len(vararr)): 
+    print vararr[i].id
+'''
+End of Physiological variables
+'''
+
 
 dfs = []
 for j in range (0, 10):
     # padj: leftmost 2 digits 
     padj = str(j).rjust(2, '0')
-    for i in range(1, 2):
+    for i in range(1, 20):
         try:
             # padj: rightmost 3 digits 
             padi = str(i).rjust(3, '0')
@@ -32,13 +72,7 @@ for j in range (0, 10):
             #file_icustayev = "{0}/ICUSTAYEVENTS-{0}.txt".format(t)
             file_icustaydet = padj + "/" + "{0}/ICUSTAY_DETAIL-{0}.txt".format(pid)
   
-    
-            TABLE_CHARTEV = pd.read_csv(get_table_mimic(padj, 'CHARTEVENTS'), header = 0, usecols=['SUBJECT_ID', 'ITEMID', 'ICUSTAY_ID', 'VALUE1NUM' ])
-           
-            TABLE_CHARTEV = TABLE_CHARTEV.query('ITEMID == {0} and VALUE1NUM >= {1} and  VALUE1NUM <= {2}'.format(v_resp.id, v_resp.min, v_resp.max ) )
-            print TABLE_CHARTEV.groupby('ICUSTAY_ID')['VALUE1NUM'].mean()
-            
-            
+
             data_tmp = pd.read_csv(file_dpatients, header = 0, parse_dates=True, usecols=['SUBJECT_ID', 'HOSPITAL_EXPIRE_FLG'])
             #data_tmp_icustays = pd.read_csv(file_icustayev, header = 0, parse_dates=True, usecols=['SUBJECT_ID', 'ICUSTAY_ID','INTIME'])  
             data_tmp_icustaydet = pd.read_csv(file_icustaydet, header = 0, parse_dates=True, usecols=['SUBJECT_ID', 'ICUSTAY_ID', 'ICUSTAY_ADMIT_AGE', 'ICUSTAY_INTIME','ICUSTAY_OUTTIME'])  
@@ -54,12 +88,30 @@ for j in range (0, 10):
                 print e2
             data_merged = df.query('ICUSTAY_ADMIT_AGE>15 and STAY>0')
             #data_merged = data_merged.merge(TABLE_CHARTEV,on=['ICUSTAY_ID'], how='inner').groupby(['ICUSTAY_ID'], as_index=False)['VALUE1NUM'].mean()
+            
+            
+            '''
+            accumulate only resp variable
+            TABLE_CHARTEV = TABLE_CHARTEV.query('ITEMID == {0} and VALUE1NUM >= {1} and  VALUE1NUM <= {2}'.format(physivar.id, physivar.min, physivar.max ) )
+            #print TABLE_CHARTEV.groupby('ICUSTAY_ID')['VALUE1NUM'].mean()
             aggregated = TABLE_CHARTEV.groupby('ICUSTAY_ID').mean()['VALUE1NUM']
             aggregated.name = 'RESP'
-            print "***********************$$$$"
-            print aggregated
-            print "***********************"
+            #print "***********************$$$$"
+            #print aggregated
+            #print "***********************"
             data_merged = data_merged.join(aggregated,on='ICUSTAY_ID')
+            '''
+            TABLE_CHARTEV = pd.read_csv(get_table_mimic(padj, 'CHARTEVENTS'), header = 0, usecols=['SUBJECT_ID', 'ITEMID', 'ICUSTAY_ID', 'VALUE1NUM' ])
+            
+            
+            for physivar in vararr: 
+                aggregated = TABLE_CHARTEV.query('ITEMID == {0} and VALUE1NUM >= {1} and  VALUE1NUM <= {2}'.format(physivar.id, physivar.min, physivar.max ) )
+                aggregated = aggregated.groupby('ICUSTAY_ID').mean()['VALUE1NUM']
+                aggregated.name = physivar.name
+                #print "***********************$$$$"
+                #print aggregated
+                #print "***********************"
+                data_merged = data_merged.join(aggregated, on='ICUSTAY_ID')
             
             
             del df
@@ -70,17 +122,49 @@ del data_tmp
 #del data_tmp_icustays
 del data_merged
 data = pd.concat(dfs)
-del data['ICUSTAY_OUTTIME']
-del data['ICUSTAY_INTIME']
+#del data['ICUSTAY_OUTTIME']
+#del data['ICUSTAY_INTIME']
 print data
 print "Total data: {0}".format(data.shape[0])
 data = data.query('HOSPITAL_EXPIRE_FLG == \'N\'')
 print "Total data, after expire: {0}".format(data.shape[0])
 
-#data.to_csv('filtered1.csv')
+data.to_csv('all.csv')
+
+no_read = data.groupby(['SUBJECT_ID'])
+no_read = no_read.filter(lambda x: len(x) < 2)
+no_read['CLASS'] = no_read.apply(lambda x: filler(0), axis = 1)
+#no_read = data[data.groupby('ICUSTAY_ID').pid.transform(len) > 1]
+print no_read
+print "Total not readmit,  {0}".format(no_read.shape[0])
+no_read.to_csv('negative.csv')
 
 
 
+#readmitted =  data.groupby(['SUBJECT_ID'])
+#data['ICUSTAY_INTIME'] = data.apply(lambda x: str(x['ICUSTAY_INTIME']), axis = 1)
+#readmitted = data.groupby('SUBJECT_ID').min()['ICUSTAY_ID']  
+
+readmitted = data.groupby(['SUBJECT_ID']).filter(lambda x: len(x) > 1)
+print readmitted
+readmitted = readmitted.sort_values('ICUSTAY_ID', ascending=True).drop_duplicates(['SUBJECT_ID'])
+readmitted['CLASS'] = readmitted.apply(lambda x: filler(1), axis = 1)
+#readmitted = data.join(readmitted, on='ICUSTAY_ID')
+print readmitted
+
+
+
+readmitted.to_csv('positive.csv')
+mimicall = []
+mimicall.append(no_read)
+mimicall.append(readmitted)
+finaldata = pd.concat(mimicall)
+
+finaldata.drop(finaldata.columns[[0, 1, 2, 3, 4, 5, 6]], axis=1, inplace=True)  # df.columns is zero-based pd.Index 
+#finaldata.drop(['ICUSTAY_OUTTIME', 'ICUSTAY_INTIME', 'HOSPITAL_EXPIRE_FLG', 'ICUSTAY_ID', 'SUBJECT_ID', 'STAY' ], axis=1, inplace=True)
+
+
+finaldata.to_csv('finaldata.csv', index = False)
 '''
 ### HELPING CODES
 
